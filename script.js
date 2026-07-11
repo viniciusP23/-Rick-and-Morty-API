f332ddd95646873d831ec43427850370bdf9034c
viniciusP23 <vinicius_pereira1@live.com>
2026-07-10 21:32:21 -0300

    filtrando informações da API

--- script.js
+++ script.js
@@ -14,13 +14,24 @@ const fetchApi = (value) => {
     return result
 }
 
+const keys = ["name", "status", "species", "gender", "origin", "episode"]
+
+const buildResult = (result) => {
+    const newObject = {}
+    keys.map((key) => document.getElementById(key))
+    .map((item) => {
+        item.checked && (newObject[item.name] = result[item.name])
+    })
+
+    return newObject
+}
+
 
 btnGo.addEventListener("click", async (event) => {
     event.preventDefault()
 
     const result = await fetchApi(characterId.value)
 
-    content.textContent = `${JSON.stringify(result, undefined, 2)}`
-
+    content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`
     image.src = `${result.image}`
 })
